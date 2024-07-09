51cf08cb2ba488095b3ffd5356e4549845ab048e
subhnesh2310 <subkumar@infinera.com>
2024-07-10 01:32:07 +0530

    Update create_token.py

--- cloud-pixi/create_token.py
+++ cloud-pixi/create_token.py
@@ -3,10 +3,10 @@ from google.oauth2.service_account import Credentials
 from google.cloud import bigquery
 
 # Vault server URL and path to your secret
-vault_url = "http://10.100.112.125:8200"
-secret_path = "v1/cpixi-storage/v1-secret"
-role_id = "29ba25e9-fcfe-a378-e44b-1a7232c35bee"
-secret_id = "ae6cfb91-501d-25d0-8e96-519d19f24c14"
+vault_url = ""
+secret_path = ""
+role_id = ""
+secret_id = ""
 
 def vault_authenticate(vault_url, role_id, secret_id):
     auth_url = f"{vault_url}/v1/auth/approle/login" # auth with approle
